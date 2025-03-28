88408e9d5eedc5426daa6017515bd382fc35931b
Sebastian Kootz <skxxtz@gmail.com>
2025-03-28 22:31:03 +0100

    fix: fixed incorrect path in packager.sh

--- packager.sh
+++ packager.sh
@@ -3,7 +3,7 @@ read -p "Current version: " version
 rm -rf ~/.tmp/wayland-clipboard-manager-release/
 mkdir -p ~/.tmp/wayland-clipboard-manager-release/
 cargo build --release
-cp target/release/sherlock ~/.tmp/wayland-clipboard-manager-release/
+cp target/release/wayland-clipboard-manager ~/.tmp/wayland-clipboard-manager-release/
 cp LICENSE ~/.tmp/wayland-clipboard-manager-release/LICENSE
 
 cd ~/.tmp/wayland-clipboard-manager-release/
