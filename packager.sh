e57ef27635e32ca164d10cd764aa6772395de031
Sebastian Kootz <skxxtz@gmail.com>
2025-03-28 22:26:22 +0100

    feat: added packager script chore: added license

---
+++ packager.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+read -p "Current version: " version
+rm -rf ~/.tmp/wayland-clipboard-manager-release/
+mkdir -p ~/.tmp/wayland-clipboard-manager-release/
+cargo build --release
+cp target/release/sherlock ~/.tmp/wayland-clipboard-manager-release/
+cp LICENSE ~/.tmp/wayland-clipboard-manager-release/LICENSE
+
+cd ~/.tmp/wayland-clipboard-manager-release/
+tar -czf wayland-clipboard-manager-v${version}-bin-linux-x86_64.tar.gz wayland-clipboard-manager LICENSE
+
+
